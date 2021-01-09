ed47cedc194fd40e14f27e8c2ee9e2b599c11292
Nicholas Allam <njallam@outlook.com>
2021-01-09 11:16:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const lengths = {
+  pomodoro: 25 * 60,
+  shortBreak: 5 * 60,
+  longBreak: 10 * 60,
+}
+
+let mode;
+let remainingTime;
+let interval;
+
+document.querySelector('.btn-group').addEventListener('click', function (event) {
+  const { mode: newMode } = event.target.dataset;
+  if (!newMode) return;
+
+  mode = newMode;
+  remainingTime = lengths[mode];
+
+  document.querySelectorAll('button[data-mode]').forEach(e => e.classList.remove('active'));
+  document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
+  document.querySelector('progress').setAttribute('max', remainingTime);
+
+  startTimer();
+});
+
+function updateClock() {
+  const minutes = `${Number.parseInt(remainingTime / 60)}`.padStart(2, '0');
+  const seconds = `${Number.parseInt(remainingTime % 60)}`.padStart(2, '0');
+  const time = `${minutes}:${seconds}`;
+
+  document.getElementById('clock').textContent = time;
+
+  const text = mode === 'pomodoro' ? 'until break' : 'left in break';
+  document.title = `${time} ${text}`;
+  document.getElementById('text').textContent = text;
+
+  const progress = document.querySelector('progress');
+  progress.value = Number.parseInt(lengths[mode] - remainingTime);
+}
+
+function startTimer() {
+  const endTime = Date.now() + remainingTime * 1000;
+
+  updateClock();
+  clearInterval(interval);
+  interval = setInterval(function () {
+    remainingTime = (endTime - Date.now()) / 1000;
+    updateClock();
+    if (remainingTime <= 0) clearInterval(interval);
+  }, 1000);
+}
